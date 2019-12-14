0ec92645b41d2b533d5c10b8ae36bd2ac4f4d471
girayaThiago <12597381+girayaThiago@users.noreply.github.com>
2019-12-14 05:54:06 -0300

    Light Scrip.
    Criado template para novas cenas no MacOS.
    Adicionado script de controle da luz (senoide + cor).

---
+++ Assets/Scripts/LightScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+
+public class LightScript : MonoBehaviour
+{
+    private Light2D luz;
+    public float amplitude = 2;
+    public float raio = 3;
+    public float periodo = 4;
+    public Color cor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        luz = GetComponent<Light2D>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        float t = Time.fixedTime * Mathf.PI / periodo;
+        float sin = Mathf.Sin(t);
+
+        luz.pointLightOuterRadius = raio + (amplitude * sin * sin);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            luz.color = cor;
+        }
+        else if (Input.GetKey(KeyCode.R))
+        {
+            luz.color = Color.red;
+        }
+
+    }
+}
