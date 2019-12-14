e29f3d1da6bf0ba472d2110efcc459c1ada22029
girayaThiago <12597381+girayaThiago@users.noreply.github.com>
2019-12-14 01:04:34 -0300

    Added PlaceholderMenu
    Todo: Options

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Options()
+    {
+        
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
