564d8acc8fc3e6dfb07860c5d33f52f81e9fd010
Aendur <Aendur@users.noreply.github.com>
2019-12-13 22:48:54 -0300

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Rigidbody2D rigidBody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float dx = Input.GetAxis("Horizontal");
+        float dy = Input.GetAxis("Vertical");
+
+        //rigidBody.velocity = new Vector2(dx, dy).normalized * 5.0f;
+        transform.Translate(dx, dy, 0);
     }
 }
