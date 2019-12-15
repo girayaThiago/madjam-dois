4e4aedbf8fc6b1bd677b889e73f702f86aa0f802
Aendur <Aendur@users.noreply.github.com>
2019-12-15 04:14:08 -0300

    supernova

---
+++ Assets/Sprites/Supernova.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class Supernova : MonoBehaviour
+{
+    float K;
+    Collider collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        K = 1000.0f;
+        collider = GetComponent<Collider>();
+    }
+
+    IEnumerator OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject player = collision.collider.gameObject;
+        Vector2 v2 = player.transform.position - transform.position;
+        player.GetComponent<Rigidbody2D>().AddForce(v2 * K);
+        player.GetComponent<Rigidbody2D>().AddTorque(10.0f);
+        transform.localScale *= 1.5f;
+        GetComponent<Animator>().Play("supernova_explode");
+        yield return new WaitForSecondsRealtime(1);
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
